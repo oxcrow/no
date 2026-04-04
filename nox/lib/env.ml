a64a506ef3aa8a62d81608170c5dc22153f9b1cc
oxcrow <oxcrow@proton.me>
2026-04-04 17:27:21 +0530

    xxx:

--- nox/lib/env.ml
+++ nox/lib/env.ml
@@ -1,6 +1,6 @@
 open Core
 
-type env = { table : (string, record) Hashtbl.t }
+type env = { table : (string, record) Hashtbl.t; scope : string list list }
 
 and record =
   | Function of { name : string; type' : Ast.types; loc : Ast.loc }
@@ -10,8 +10,7 @@ and record =
 
 let isFunction x = match x with Ast.Function _ -> true | _ -> false
 
-(* *)
-let addEntities entities =
+let addEntities (entities : Ast.entities list) =
   let rec aux entities env =
     match entities with
     | [] -> env
@@ -21,11 +20,19 @@ let addEntities entities =
           | Ast.Function f -> (Get.Ast.name f.name, Get.Ast.locOfName f.name)
           | _ -> xTODO uPOS "add-entity-to-environment"
         in
-        Hashtbl.replace env.table name (Function { name; type' = Ast.UnitType; loc });
+        let type' =
+          match head with
+          | Ast.Function f -> f.type'
+          | _ -> xTODO uPOS "add-entity-type-to-environment"
+        in
+        Hashtbl.replace env.table name (Function { name; type'; loc });
         aux tail env
   in
-  aux entities { table = Hashtbl.create 100 }
+  aux entities { table = Hashtbl.create 100; scope = [] }
 ;;
 
-(* *)
+let addScope (env : env) = { env with scope = [] :: env.scope }
+let removeScope (env : env) = { env with scope = List.tl env.scope }
+
+(* Initialize an environment of a file, by storing its entities in it *)
 let initFileEnv ast = match ast with Ast.File f -> addEntities f.entities
