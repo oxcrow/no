efd4bc21934301c38cc8229f7bd5093d437244f1
oxcrow <oxcrow@proton.me>
2026-04-05 20:49:04 +0530

    x

--- nox/lib/analyser.ml
+++ nox/lib/analyser.ml
@@ -3,10 +3,23 @@ open Core
 let isFunction x = match x with Ast.Function _ -> true | _ -> false
 
 (** Analyse function and lower it to IR *)
-let analyseFunction (env : Env.env) (entity : Ast.entities) =
+let analyseFunction (file : string) (env : Env.env) (entity : Ast.entities) =
   match entity with
   | Ast.Function f -> print_endline (Get.Ast.name f.name)
-  | _ -> xTODO uPOS "analyze-entity"
+  | _ ->
+      raise
+        (Report
+           {
+             message = "Unable to analyze entity.";
+             source = xSOURCE uPOS;
+             error =
+               (let lineIdx, colIdx =
+                  match Get.Ast.locofEntity entity with
+                  | Ast.Location l -> (l.lineIdx, l.colIdx)
+                  | Ast.Nowhere -> xNEVER uPOS "wut?"
+                in
+                Some (Any (Error.UnknownParserError (file, (lineIdx, colIdx)))));
+           })
 ;;
 
 (** Analyse file and lower it to IR *)
@@ -15,6 +28,6 @@ let analyseFile (ast : Ast.file) =
   match ast with
   | Ast.File f ->
       List.map
-        (fun entity -> analyseFunction env entity)
+        (fun entity -> analyseFunction f.file env entity)
         (List.filter isFunction f.entities)
 ;;
--- nox/lib/get.ml
+++ nox/lib/get.ml
@@ -4,4 +4,12 @@ open Core
 module Ast = struct
   let name x = match x with Ast.Name n -> n.name
   let locOfName x = match x with Ast.Name n -> n.loc
+
+  let locofEntity x =
+    match x with
+    | Ast.Function y -> y.loc
+    | Ast.Struct y -> y.loc
+    | Ast.Use y -> y.loc
+    | _ -> xTODO uPOS "loc-of-entity"
+  ;;
 end
