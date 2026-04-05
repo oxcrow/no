5f9a4c573fe3e347ce1fa49e874f3a4ec9f6a844
oxcrow <oxcrow@proton.me>
2026-04-05 20:40:00 +0530

    x

--- nox/lib/analyser.ml
+++ nox/lib/analyser.ml
@@ -3,7 +3,11 @@ open Core
 let isFunction x = match x with Ast.Function _ -> true | _ -> false
 
 (** Analyse function and lower it to IR *)
-let analyseFunction entity env = ()
+let analyseFunction (env : Env.env) (entity : Ast.entities) =
+  match entity with
+  | Ast.Function f -> print_endline (Get.Ast.name f.name)
+  | _ -> xTODO uPOS "analyze-entity"
+;;
 
 (** Analyse file and lower it to IR *)
 let analyseFile (ast : Ast.file) =
@@ -11,6 +15,6 @@ let analyseFile (ast : Ast.file) =
   match ast with
   | Ast.File f ->
       List.map
-        (fun entity -> analyseFunction entity env)
+        (fun entity -> analyseFunction env entity)
         (List.filter isFunction f.entities)
 ;;
